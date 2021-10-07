16daa2c3cddb8d3b095fc626107083285673399f
unknown <johncloeter@gmail.com>
2021-10-07 07:52:51 -0500

    finalizing context api

--- src/componenets/Header/Cart.js
+++ src/componenets/Header/Cart.js
@@ -5,9 +5,14 @@ import styles from "./Cart.module.css";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartData);
-
+  console.log(cartCtx.cartNumber === 0);
   return (
-    <Button onClick={cartCtx.onShowModal} className={styles.cart}>
+    <Button
+      onClick={cartCtx.onShowModal}
+      className={`${styles.cart} ${
+        cartCtx.cartNumber === 0 ? styles.empty : null
+      }`}
+    >
       Your Cart
       <span className={styles.cart_amount}>{cartCtx.cartNumber}</span>
     </Button>
